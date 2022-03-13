1994dadb84a3c3fa79273e62b21f79b71135e59f
David Drummer <david.b.drummer@gmail.com>
2022-03-13 13:57:36 -0400

    commit tree 6862c474ae24fcccd39bf0dc433d67f177157cd1

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    let word = string.split('');
+    word[0] = word[0].toUpperCase();
+    word = word.join('');
+    return word
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+  static titleize(string) {
+    let exceptionArray = new Set(['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']);
+    let wordArray = string.split(' ');
+    let firstWord = this.capitalize(wordArray[0]);
+    wordArray.shift();
+    let newArray = wordArray.map(word => {
+      word = exceptionArray.has(word) ? word.toLowerCase() : word[0].toUpperCase() + word.substring(1);
+      return word;
+    });
+    newArray.unshift(firstWord)
+    return newArray.join(' ');
+  }
 }
+
+//console.log(Formatter.titleize('hello there! the sun looks nice and there is an apple by the table from granny.'));
